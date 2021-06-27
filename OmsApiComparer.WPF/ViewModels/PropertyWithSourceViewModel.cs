e19e5c84fb84d8e23d1b6db871348059683e9f63
Valeri Hristov <valeri.hristov@outlook.com>
2021-06-28 00:26:24 +0200

    Updating styles

--- OmsApiComparer.WPF/ViewModels/PropertyWithSourceViewModel.cs
+++ OmsApiComparer.WPF/ViewModels/PropertyWithSourceViewModel.cs
@@ -12,7 +12,9 @@ namespace OmsApiComparer.WPF
         public bool IsCustomType =>
             Property?.Type != null && !_types.Contains(Property.Type);
 
-        public string Value => Property == null ? string.Empty : $"{(Property.IsRequired ? "Required" : "Optional")} ({Property.Type})";
+        public bool IsRequired => Property != null && Property.IsRequired;
+
+        public string Value => Property == null ? string.Empty : $"{Property.Type} {(IsRequired ? "(Required)" : "")}";
 
         private static readonly ImmutableHashSet<string> _types =
             ImmutableHashSet.Create("string", "integer", "boolean", "array", "number");
